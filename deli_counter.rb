a1b3bb8e80279842bc81df64f55770b16daeb9d2
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 00:37:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def line(line_array)
   if line_array.length > 0
     line = "The line is currently: "
     line_array.each.with_index do |customer, index|
+      binding.pry
       line << "#{index + 1}. #{customer}"
     end
     line
