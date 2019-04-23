7ed0932bcb20d85f58949c6bc845399062339232
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 00:40:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,7 @@ require 'pry'
 def line(line_array)
   if line_array.length > 0
     line = "The line is currently:"
-    line_array.each.with_index do |customer, index|
-      line << " #{index + 1}. #{customer}"
-    end
+    line_array.each.with_index { |customer, index| line << " #{index + 1}. #{customer}" }
     puts "#{line}"
   else 
     puts "The line is currently empty."
